ccdecb62e518f1e3ba70e210110bf0e4e678eb25
Arslan-the-Developer <callmearslanahmed@gmail.com>
2025-03-27 12:44:12 +0500

    Improved HeroSection

--- src/Screens/HomeScreen/Components/HeroSection.jsx
+++ src/Screens/HomeScreen/Components/HeroSection.jsx
@@ -16,9 +16,9 @@ function HeroSection() {
                 <motion.div initial={{opacity : 0, scale : 0.9}} animate={{opacity : 1, scale : 1, transition : {delay : 0.3}}} className='w-full h-full bg-[#ffffffbf] rounded-md flex flex-col items-start max-[600px]:items-center justify-center max-[600px]:justify-start px-4 py-3 relative'>
                     
                     <motion.h1 initial={{opacity : 0, y : -10}} animate={{opacity : 1, y : 0, transition : {delay : 0.5}}} className='font-semibold text-2xl max-[600px]:text-xl font-product mb-2 text-primary tracking-wide'>Winter Season Is In</motion.h1>
-                    <motion.p initial={{opacity : 0, x : 20}} animate={{opacity : 1, x : 0, transition : {delay : 0.5}}} className='w-6/10 font-product max-[600px]:text-sm max-[600px]:text-center'>Shop Now, from the Best & Verified Sellers Nationwide & Express Yourself With Your Loved Ones With Latest Fabric Trends</motion.p>
+                    <motion.p initial={{opacity : 0, x : 20}} animate={{opacity : 1, x : 0, transition : {delay : 0.5}}} className='w-6/10 font-product max-[600px]:text-sm max-[600px]:text-center max-[600px]:w-8/10'>Shop Now, from the Best & Verified Sellers Nationwide & Express Yourself With Your Loved Ones With Latest Fabric Trends</motion.p>
 
-                    <motion.button initial={{opacity : 0, scale : 0.8}} animate={{opacity : 1, scale : 1, transition : {delay : 0.5}}} className='absolute right-8 max-[600px]:right-45 max-[600px]:bottom-2 font-product border-y-2 py-2 px-4 flex items-center justify-center transition-all hover:py-3 hover:text-primary hover:border-primary'>
+                    <motion.button initial={{opacity : 0, scale : 0.8}} animate={{opacity : 1, scale : 1, transition : {delay : 0.5}}} className='absolute right-8 max-[600px]:right-[50%] max-[600px]:bottom-2 font-product border-y-2 py-2 px-4 flex items-center justify-center transition-all hover:py-3 hover:text-primary hover:border-primary'>
                         <Link>
                             Shop Now
                         </Link>
