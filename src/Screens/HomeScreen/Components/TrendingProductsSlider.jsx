fa0d245cadd8c9001c3edd00c5be362503b7514a
Arslan-the-Developer <callmearslanahmed@gmail.com>
2025-04-11 15:10:26 +0500

    Improved LatestProductsSlider in HomeScreen

--- src/Screens/HomeScreen/Components/TrendingProductsSlider.jsx
+++ src/Screens/HomeScreen/Components/TrendingProductsSlider.jsx
@@ -45,126 +45,31 @@ function TrendingProductsSlider() {
 
     <section className='w-full flex items-center justify-center mt-2 px-2'>
 
-        <Swiper className='w-full relative flex items-center justify-center h-70' slidesPerView={2} spaceBetween={15} breakpoints={{1024 : {slidesPerView : 5, spaceBetween : 5}, 960 : {slidesPerView : 4}, 640 : {slidesPerView : 3}}}>
+        <Swiper className='w-full relative flex items-center justify-center h-70' slidesPerView={2} spaceBetween={15} breakpoints={{480 : {slidesPerView : 1}, 640 : {slidesPerView : 2}, 960 : {slidesPerView : 3}, 1024 : {slidesPerView : 4}, 1180 : {slidesPerView : 5}}}>
 
-            <SwiperSlide className='flex items-center justify-center'>
+            <SwiperSlide className='w-full h-full bg-white rounded-sm shadow-sm'>
 
-                <div className='w-full h-full flex flex-col items-center justify-center bg-white rounded-sm overflow-hidden p-2 font-product' style={{minWidth : "240px", maxWidth : "240px"}}>
+                <div className='w-full h-full flex flex-col items-center justify-start'>
 
                     <div className='w-full h-6/10 flex items-center justify-center'>
 
-                        <img src="https://games4u.pk/cdn/shop/files/tfygv.jpg?v=1726053133" className='w-full h-full object-center object-contain' alt="" />
+                        <img className='w-full h-full object-center object-contain' src="https://games4u.pk/cdn/shop/files/tfygv.jpg?v=1726053133" alt="" />
 
                     </div>
+                    
+                    <div className='w-full h-4/10 flex flex-col font-product items-center justify-center' style={{minWidth : "240px"}}>
 
-                    <div className='w-full h-4/10 flex flex-col items-center justify-start py-2 font-semibold tracking-wide'>
+                        <h3 className='font-semibold'>Space Earbuds</h3>
 
-                        <h2 className='mt-2'>Space Earbuds</h2>
+                        <p className='text-sm text-primary my-1'>- Tech Mania -</p>
 
-                        <Link className='text-sm text-primary my-1'>- Tech Mania -</Link>
-
-                        <p className='text-sm mt-1'>150 Sold This Week</p>
+                        <h3>150 Sold This Week</h3>
 
                     </div>
 
                 </div>
 
             </SwiperSlide>
-            
-            <SwiperSlide className='flex items-center justify-center'>
-
-                <div className='w-full h-full flex flex-col items-center justify-center bg-white rounded-sm overflow-hidden p-2 font-product' style={{minWidth : "240px", maxWidth : "240px"}}>
-
-                    <div className='w-full h-6/10 flex items-center justify-center'>
-
-                        <img src="https://games4u.pk/cdn/shop/files/tfygv.jpg?v=1726053133" className='w-full h-full object-center object-contain' alt="" />
-
-                    </div>
-
-                    <div className='w-full h-4/10 flex flex-col items-center justify-start py-2 font-semibold tracking-wide'>
-
-                        <h2 className='mt-2'>Space Earbuds</h2>
-
-                        <Link className='text-sm text-primary my-1'>- Tech Mania -</Link>
-
-                        <p className='text-sm mt-1'>150 Sold This Week</p>
-
-                    </div>
-
-                </div>
-
-            </SwiperSlide>
-            <SwiperSlide className='flex items-center justify-center'>
-
-                <div className='w-full h-full flex flex-col items-center justify-center bg-white rounded-sm overflow-hidden p-2 font-product' style={{minWidth : "240px", maxWidth : "240px"}}>
-
-                    <div className='w-full h-6/10 flex items-center justify-center'>
-
-                        <img src="https://games4u.pk/cdn/shop/files/tfygv.jpg?v=1726053133" className='w-full h-full object-center object-contain' alt="" />
-
-                    </div>
-
-                    <div className='w-full h-4/10 flex flex-col items-center justify-start py-2 font-semibold tracking-wide'>
-
-                        <h2 className='mt-2'>Space Earbuds</h2>
-
-                        <Link className='text-sm text-primary my-1'>- Tech Mania -</Link>
-
-                        <p className='text-sm mt-1'>150 Sold This Week</p>
-
-                    </div>
-
-                </div>
-
-            </SwiperSlide>
-            <SwiperSlide className='flex items-center justify-center'>
-
-                <div className='w-full h-full flex flex-col items-center justify-center bg-white rounded-sm overflow-hidden p-2 font-product' style={{minWidth : "240px", maxWidth : "240px"}}>
-
-                    <div className='w-full h-6/10 flex items-center justify-center'>
-
-                        <img src="https://games4u.pk/cdn/shop/files/tfygv.jpg?v=1726053133" className='w-full h-full object-center object-contain' alt="" />
-
-                    </div>
-
-                    <div className='w-full h-4/10 flex flex-col items-center justify-start py-2 font-semibold tracking-wide'>
-
-                        <h2 className='mt-2'>Space Earbuds</h2>
-
-                        <Link className='text-sm text-primary my-1'>- Tech Mania -</Link>
-
-                        <p className='text-sm mt-1'>150 Sold This Week</p>
-
-                    </div>
-
-                </div>
-
-            </SwiperSlide>
-            <SwiperSlide className='flex items-center justify-center'>
-
-                <div className='w-full h-full flex flex-col items-center justify-center bg-white rounded-sm overflow-hidden p-2 font-product' style={{minWidth : "240px", maxWidth : "240px"}}>
-
-                    <div className='w-full h-6/10 flex items-center justify-center'>
-
-                        <img src="https://games4u.pk/cdn/shop/files/tfygv.jpg?v=1726053133" className='w-full h-full object-center object-contain' alt="" />
-
-                    </div>
-
-                    <div className='w-full h-4/10 flex flex-col items-center justify-start py-2 font-semibold tracking-wide'>
-
-                        <h2 className='mt-2'>Space Earbuds</h2>
-
-                        <Link className='text-sm text-primary my-1'>- Tech Mania -</Link>
-
-                        <p className='text-sm mt-1'>150 Sold This Week</p>
-
-                    </div>
-
-                </div>
-
-            </SwiperSlide>
-
-
 
         </Swiper>
 
