8f47a23b08bf23def1c1c3adabd66cd3c0650c04
Arslan-the-Developer <callmearslanahmed@gmail.com>
2025-04-19 14:52:23 +0500

    changed the size of icons on product card in responsive

--- src/Screens/CategoryProductsScreen/CategoryProductsScreen.jsx
+++ src/Screens/CategoryProductsScreen/CategoryProductsScreen.jsx
@@ -51,34 +51,38 @@ function CategoryProductsScreen() {
 
     <>
         <Navbar />
-        <section className={`h-screen w-full flex flex-col items-start justify-start content-start py-18 px-10`} style={{height : "90vh"}}>
+        <section className={`h-screen w-full flex flex-col items-start justify-start content-start py-18 px-10 max-md:px-2`} style={{height : "90vh"}}>
 
-            <h2 className='font-product text-primary font-semibold tracking-wider mb-6'>Products - {productSubCategory}</h2>
+            <h2 className='font-product text-primary font-semibold tracking-wider mb-6 max-md:ml-5'>Products - {productSubCategory}</h2>
 
-            {
-                productsLoading ? (
+            <div className='w-full flex flex-wrap items-start justify-start'>
 
-                    <div className='w-full h-full flex flex-col items-center justify-center'>
-                        <PuffLoader color='#006964' />
-                        <h2 className='mt-4 font-product text-xl text-primary'>Loading Products...</h2>
-                        <h2 className='mt-2'>{productSubCategory}</h2>
-                    </div>
+                {
+                    productsLoading ? (
 
-                ) : (
-
-                    productsCount < 1 ? (
                         <div className='w-full h-full flex flex-col items-center justify-center'>
-                            <h2 className='text-xl font-product text-primary'>No Products For This Category</h2>
-                            <Link to={'/'} className='font-product mt-4 text-primary px-6 py-3 border-2 border-primary transition hover:bg-primary hover:text-secondary'>Back To Home</Link>
+                            <PuffLoader color='#006964' />
+                            <h2 className='mt-4 font-product text-xl text-primary'>Loading Products...</h2>
+                            <h2 className='mt-2'>{productSubCategory}</h2>
                         </div>
-                    ) : (
-                        products.map((product) => (
-                            <ProductCard product={product} />
-                        ))
-                    )
 
-                )
-            }
+                    ) : (
+
+                        productsCount < 1 ? (
+                            <div className='w-full h-full flex flex-col items-center justify-center'>
+                                <h2 className='text-xl font-product text-primary'>No Products For This Category</h2>
+                                <Link to={'/'} className='font-product mt-4 text-primary px-6 py-3 border-2 border-primary transition hover:bg-primary hover:text-secondary'>Back To Home</Link>
+                            </div>
+                        ) : (
+                            products.map((product) => (
+                                <ProductCard product={product} />
+                            ))
+                        )
+
+                    )
+                }
+            
+            </div>
 
         </section>
     </>
