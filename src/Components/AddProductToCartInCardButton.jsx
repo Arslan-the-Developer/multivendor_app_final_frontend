91e78ab31caa6850f652e4a96a640576e7d54ad7
Arslan-the-Developer <callmearslanahmed@gmail.com>
2025-04-19 15:05:41 +0500

    Improved Product Card

--- src/Components/AddProductToCartInCardButton.jsx
+++ src/Components/AddProductToCartInCardButton.jsx
@@ -42,8 +42,8 @@ function AddProductToCartButtonInCard({product, AddToCartFunction}) {
           <Link to={"/user-cart"} className="w-full h-full flex items-center justify-center" title="View In Cart">
               <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  width={windowDimensions.width <= 450 ? 15 : 24}
-                  height={windowDimensions.width <= 450 ? 15 : 24}
+                  width={windowDimensions.width <= 450 ? 19 : 24}
+                  height={windowDimensions.width <= 450 ? 19 : 24}
                   viewBox="0 0 24 24"
                   fill="none"
                   className="injected-svg"
@@ -60,7 +60,7 @@ function AddProductToCartButtonInCard({product, AddToCartFunction}) {
 
       ) : (
 
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={windowDimensions.width <= 450 ? 15 : 24} height={windowDimensions.width <= 450 ? 15 : 24} color={"#006964"} fill={"none"}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={windowDimensions.width <= 450 ? 19 : 24} height={windowDimensions.width <= 450 ? 19 : 24} color={"#006964"} fill={"none"}>
               <path d="M13 18H21M17 22L17 14" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
               <path d="M7 7.5V7C7 4.23858 9.23858 2 12 2C14.7614 2 17 4.23858 17 7V7.5" stroke="currentColor" strokeWidth="1.5" />
               <path d="M10 22C7.71999 21.9999 6.57085 21.9917 5.76809 21.2752C4.95603 20.5505 4.75097 19.3264 4.34085 16.8781L3.17786 9.93557C2.98869 8.8063 2.89411 8.24167 3.18537 7.87083C3.47662 7.5 4.01468 7.5 5.09079 7.5H18.9092C19.9853 7.5 20.5234 7.5 20.8146 7.87083C21.1059 8.24167 21.0113 8.8063 20.8221 9.93557L20.4763 12" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
