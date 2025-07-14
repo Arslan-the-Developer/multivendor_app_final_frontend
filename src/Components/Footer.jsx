e9be5ca095743f016b10bc513306d9b3d237dc3f
Arslan-the-Developer <callmearslanahmed@gmail.com>
2025-07-14 09:07:10 +0500

    Made Footer Responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,13 +8,13 @@ function Footer() {
 
     <footer className='w-full h-100 bg-[#eaeaea] border-2 border-gray-300 flex flex-col items-center justify-center mt-10 rounded-t-3xl overflow-hidden'>
 
-        <div className='w-full h-full flex items-start justify-start'>
+        <div className='w-full h-full flex items-start justify-start max-sm:flex-col'>
 
-            <div className='w-1/3 h-full flex flex-col items-center justify-start p-8'>
+            <div className='w-1/3 h-full flex flex-col items-center justify-start p-8 max-sm:w-full max-sm:p-4 max-sm:mt-3'>
 
                 <h2 className='text-2xl font-product tracking-wide font-semibold text-primary'>Contact Us</h2>
 
-                <div className='flex flex-col items-start justify-start font-product'>
+                <div className='flex min-sm:flex-col items-start justify-start font-product max-sm:w-full max-sm:justify-around'>
                     <div className='flex items-center justify-center mt-10'>
 
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={32} height={32} color={"#006964"} fill={"none"}>
@@ -51,46 +51,46 @@ function Footer() {
             </div>
 
 
-            <div className='w-1/3 h-full flex flex-col items-center justify-start p-8'>
+            <div className='w-1/3 h-full flex flex-col items-center justify-start p-8 max-sm:w-full max-sm:p-4'>
 
                 <h2 className='text-2xl font-product tracking-wide font-semibold text-primary'>Support</h2>
 
-                <div className='flex flex-col items-center justify-start mt-10 text-primary font-product'>
+                <div className='flex min-sm:flex-col items-center justify-start mt-10 text-primary font-product max-sm:justify-around max-sm:mt-6'>
 
                     <Link to={'/supprot/seller-tutorial'} className='px-2 border-x-2 border-transparent transition-all hover:border-primary tracking-wide hover:tracking-normal'>Seller Tutorial</Link>
 
-                    <Link to={'/supprot/buy-the-best'} className='px-2 border-x-2 border-transparent transition-all hover:border-primary mt-4 tracking-wide hover:tracking-normal'>Buy The Best</Link>
+                    <Link to={'/supprot/buy-the-best'} className='px-2 border-x-2 border-transparent transition-all hover:border-primary min-sm:mt-4 tracking-wide hover:tracking-normal'>Buy The Best</Link>
 
-                    <Link to={'/supprot/privacy-policy'} className='px-2 border-x-2 border-transparent transition-all hover:border-primary mt-4 tracking-wide hover:tracking-normal'>Privacy Policy</Link>
+                    <Link to={'/supprot/privacy-policy'} className='px-2 border-x-2 border-transparent transition-all hover:border-primary min-sm:mt-4 tracking-wide hover:tracking-normal'>Privacy Policy</Link>
 
-                    <Link to={'/supprot/terms-of-use'} className='px-2 border-x-2 border-transparent transition-all hover:border-primary mt-4 tracking-wide hover:tracking-normal'>Terms Of Use</Link>
+                    <Link to={'/supprot/terms-of-use'} className='px-2 border-x-2 border-transparent transition-all hover:border-primary min-sm:mt-4 tracking-wide hover:tracking-normal'>Terms Of Use</Link>
 
                 </div>
 
             </div>
             
-            <div className='w-1/3 h-full flex flex-col items-center justify-start p-8'>
+            <div className='w-1/3 h-full flex flex-col items-center justify-start p-8 max-sm:p-2 max-sm:w-full'>
 
-                <h2 className='text-2xl font-product tracking-wide text-primary font-semibold'>Follow Us</h2>
+                <h2 className='text-2xl font-product tracking-wide text-primary font-semibold max-sm:hidden'>Follow Us</h2>
 
-                <div className='flex flex-col items-center justify-start mt-10 text-white'>
+                <div className='flex min-sm:flex-col items-center justify-start mt-10 text-white max-sm:mt-0 max-sm:justify-center max-sm:w-full'>
 
-                    <div className='flex items-center justify-between'>
+                    <div className='flex items-center justify-between max-sm:w-1/3 max-sm:justify-around max-sm:scale-80'>
                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 48 48">
                             <linearGradient id="Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1" x1="9.993" x2="40.615" y1="9.993" y2="40.615" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#2aa4f4"></stop><stop offset="1" stop-color="#007ad9"></stop></linearGradient><path fill="url(#Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1)" d="M24,4C12.954,4,4,12.954,4,24s8.954,20,20,20s20-8.954,20-20S35.046,4,24,4z"></path><path fill="#fff" d="M26.707,29.301h5.176l0.813-5.258h-5.989v-2.874c0-2.184,0.714-4.121,2.757-4.121h3.283V12.46 c-0.577-0.078-1.797-0.248-4.102-0.248c-4.814,0-7.636,2.542-7.636,8.334v3.498H16.06v5.258h4.948v14.452 C21.988,43.9,22.981,44,24,44c0.921,0,1.82-0.084,2.707-0.204V29.301z"></path>
                         </svg>
                     
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 48 48" className='ml-3'>
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 48 48" className='ml-3 max-sm:ml-0'>
                             <radialGradient id="yOrnnhliCrdS2gy~4tD8ma_Xy10Jcu1L2Su_gr1" cx="19.38" cy="42.035" r="44.899" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fd5"></stop><stop offset=".328" stop-color="#ff543f"></stop><stop offset=".348" stop-color="#fc5245"></stop><stop offset=".504" stop-color="#e64771"></stop><stop offset=".643" stop-color="#d53e91"></stop><stop offset=".761" stop-color="#cc39a4"></stop><stop offset=".841" stop-color="#c837ab"></stop></radialGradient><path fill="url(#yOrnnhliCrdS2gy~4tD8ma_Xy10Jcu1L2Su_gr1)" d="M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z"></path><radialGradient id="yOrnnhliCrdS2gy~4tD8mb_Xy10Jcu1L2Su_gr2" cx="11.786" cy="5.54" r="29.813" gradientTransform="matrix(1 0 0 .6663 0 1.849)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#4168c9"></stop><stop offset=".999" stop-color="#4168c9" stop-opacity="0"></stop></radialGradient><path fill="url(#yOrnnhliCrdS2gy~4tD8mb_Xy10Jcu1L2Su_gr2)" d="M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z"></path><path fill="#fff" d="M24,31c-3.859,0-7-3.14-7-7s3.141-7,7-7s7,3.14,7,7S27.859,31,24,31z M24,19c-2.757,0-5,2.243-5,5	s2.243,5,5,5s5-2.243,5-5S26.757,19,24,19z"></path><circle cx="31.5" cy="16.5" r="1.5" fill="#fff"></circle><path fill="#fff" d="M30,37H18c-3.859,0-7-3.14-7-7V18c0-3.86,3.141-7,7-7h12c3.859,0,7,3.14,7,7v12	C37,33.86,33.859,37,30,37z M18,13c-2.757,0-5,2.243-5,5v12c0,2.757,2.243,5,5,5h12c2.757,0,5-2.243,5-5V18c0-2.757-2.243-5-5-5H18z"></path>
                         </svg>
                     </div>
                     
-                    <div className='flex items-center justify-between mt-5'>
+                    <div className='flex items-center justify-between mt-5 max-sm:mt-0 max-sm:w-1/3 max-sm:justify-around max-sm:scale-80'>
                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 50 50">
                             <path d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z"></path>
                         </svg>
                     
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 48 48" className='ml-3'>
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 48 48" className='ml-3 max-sm:ml-0'>
                             <path fill="#FF3D00" d="M43.2,33.9c-0.4,2.1-2.1,3.7-4.2,4c-3.3,0.5-8.8,1.1-15,1.1c-6.1,0-11.6-0.6-15-1.1c-2.1-0.3-3.8-1.9-4.2-4C4.4,31.6,4,28.2,4,24c0-4.2,0.4-7.6,0.8-9.9c0.4-2.1,2.1-3.7,4.2-4C12.3,9.6,17.8,9,24,9c6.2,0,11.6,0.6,15,1.1c2.1,0.3,3.8,1.9,4.2,4c0.4,2.3,0.9,5.7,0.9,9.9C44,28.2,43.6,31.6,43.2,33.9z"></path><path fill="#FFF" d="M20 31L20 17 32 24z"></path>
                         </svg>
                     </div>
